d31badf3d4e09c2c1196e39a627520d1dbf525c8
Doris <Preston.Doris@paradigmcorp.com>
2019-11-11 13:33:03 -0800

    In side menu during collapsed mode, hovering shows labels now

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,45 +3,45 @@
     <b-nav-text id="logoName" >
       <div class="nav-link">
         <img src="@/assets/fuge.png" alt="logo">
-        <h3 v-bind:class="[$store.state.sideCollapsed ? activeClass : fwLink]">Fugeware</h3>
+        <h3 v-bind:class="[$store.state.sideCollapsed  && !$store.state.sideCollapsedHover ? activeClass : fwLink]">Fugeware</h3>
       </div>
     </b-nav-text>
     <Divider />
     <b-nav-text class="b-nav-item b-nav-text">
         <div class="nav-link">
           <i class="fas fa-user-cog icon"></i>
-          <h4 v-bind:class="[$store.state.sideCollapsed ? activeClass : fwLink]">Preston Doris</h4>
+          <h4 v-bind:class="[$store.state.sideCollapsed  && !$store.state.sideCollapsedHover ? activeClass : fwLink]">Preston Doris</h4>
         </div>
     </b-nav-text>
     <Divider />
     <b-nav-item class="b-nav-item b-nav">
       <router-link class="nav-link " to="/">
         <i class="fas fa-chart-pie icon"></i>
-        <h5 v-bind:class="[$store.state.sideCollapsed ? activeClass : fwLink]">Dashboard</h5>
+        <h5 v-bind:class="[$store.state.sideCollapsed  && !$store.state.sideCollapsedHover ? activeClass : fwLink]">Dashboard</h5>
       </router-link>
     </b-nav-item>
     <b-nav-item class="b-nav-item b-nav">
       <router-link class="nav-link" to="/brands">
         <i class="fas fa-briefcase icon"></i>
-        <h5 v-bind:class="[$store.state.sideCollapsed ? activeClass : fwLink]">Brands</h5>
+        <h5 v-bind:class="[$store.state.sideCollapsed && !$store.state.sideCollapsedHover ? activeClass : fwLink]">Brands</h5>
       </router-link>
     </b-nav-item>
     <b-nav-item class="b-nav-item b-nav">
       <router-link class="nav-link" to="/fermentations">
         <i class="fas fa-chart-area icon"></i>
-        <h5 v-bind:class="[$store.state.sideCollapsed ? activeClass : fwLink]">Fermentations</h5>
+        <h5 v-bind:class="[$store.state.sideCollapsed  && !$store.state.sideCollapsedHover ? activeClass : fwLink]">Fermentations</h5>
       </router-link>
     </b-nav-item>
     <b-nav-item class="b-nav-item b-nav">
       <router-link class="nav-link" to="/brew">
         <i class="fas fa-beer icon"></i>
-        <h5 v-bind:class="[$store.state.sideCollapsed ? activeClass : fwLink]">Brew</h5>
+        <h5 v-bind:class="[$store.state.sideCollapsed  && !$store.state.sideCollapsedHover ? activeClass : fwLink]">Brew</h5>
       </router-link>
     </b-nav-item>
     <b-nav-item class="b-nav-item b-nav">
       <router-link class="nav-link" to="/batches">
         <i class="fas fa-clipboard-list icon"></i>
-        <h5 v-bind:class="[$store.state.sideCollapsed ? activeClass : fwLink]">Batches</h5>
+        <h5 v-bind:class="[$store.state.sideCollapsed  && !$store.state.sideCollapsedHover ? activeClass : fwLink]">Batches</h5>
       </router-link>
     </b-nav-item>
   </b-nav>
